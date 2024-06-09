9096faffe0b19297c966d9ab0145acbd479044e2
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-06-10 00:36:30 +0530

    feat: Implement to-do deletion

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,14 @@ export default function App() {
     })
   }
 
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter((todo) => {
+        return todo.id !== id
+      })
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -43,7 +51,7 @@ export default function App() {
                 <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
             </li>
           )
         })}
