fd74ee460b583973055e8a9332b9e8fa0a87a29d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-06-10 00:19:16 +0530

    refactor: Wrap jsx in parenthesis to reduce errors

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,15 @@ export default function App() {
       <h1 className="header">Todo List</h1>
       <ul className="list">
         {todos.map(todo => {
-          return <li>
-            <label>
-              <input type="checkbox" checked={todo.completed}/>
-              {todo.title}
-            </label>
-            <button className="btn btn-danger">Delete</button>
-          </li>
+          return (
+            <li>
+              <label>
+                <input type="checkbox" checked={todo.completed}/>
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
         })}
       </ul>
     </>
