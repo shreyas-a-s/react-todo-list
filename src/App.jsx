65b169d6fdf009003b5e4069bec209a78aa1717f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-06-09 23:47:06 +0530

    feat: Add fragment to wrap form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./styles.css"
 
 export default function App() {
-  return <form className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input type="text" id="item"/>
-    </div>
-    <button className="btn">Add</button>
-  </form>
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+    </>
+  )
 }
