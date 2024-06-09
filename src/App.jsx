4114eeb017a980cc926a0abd814dd11c4e009186
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-06-10 00:16:17 +0530

    feat: Implement basic to-do list insertion

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import "./styles.css"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodos((currentTodos) => {
+      return [...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false }]
+    })
+  }
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
@@ -14,20 +23,15 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox"/>
-            item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox"/>
-            item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return <li>
+            <label>
+              <input type="checkbox" checked={todo.completed}/>
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        })}
       </ul>
     </>
   )
